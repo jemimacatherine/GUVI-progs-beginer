1c7560fc21fa39f8268041d0515f582738131de1
jemimacatherine <jemimacatherine@gmail.com>
2015-12-11 20:45:19 +0530

    Create evnodd.c

---
+++ evnodd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int inum  ;
+printf("enter the number")  ;
+scanf("%d",&inum);
+if(inum%2==0)
+printf("its a even num");
+else
+printf("an odd number");
+return 0;
+}
