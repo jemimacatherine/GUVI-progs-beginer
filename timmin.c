4a653812c0751eb1c4c79e9b3f70794c5ff3b04a
jemimacatherine <jemimacatherine@gmail.com>
2015-12-12 20:46:50 +0530

    Update timmin.c

--- timmin.c
+++ timmin.c
@@ -1,11 +1,30 @@
 #include<stdio.h>
+
 int main()
+
 {
-int hour,min,tmin;
-printf("enter the time as hh:mm without typing colon");
-scanf("%d%d",&hour,&min);
-tmin=hour*60;
-tmin=tmin+min;
-printf("the time in minutes is %d",tmin);
+
+int hour1,min1,tmin1,hour2,min2,tmin2,leftmin;
+
+printf("enter the  first time as hh:mm without typing colon");
+
+scanf("%d%d",&hour1,&min1);
+
+tmin1=hour1*60;
+
+tmin1=tmin1+min1;
+
+
+printf("enter the  second time as hh:mm without typing colon");
+scanf("%d%d",&hour2,&min2);
+
+tmin2=hour2*60;
+
+tmin2=tmin2+min2;
+
+
+leftmin=tmin2-tmin1;
+printf("the min difference between 2time is %d",leftmin);
 return 0;
+
 }
