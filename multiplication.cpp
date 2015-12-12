30984099797e69c9e0b949302c2dc797973c1b3d
jemimacatherine <jemimacatherine@gmail.com>
2015-12-12 13:22:40 +0530

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    int a=0,b=0;
+    cout<<"Enter the Number";
+    cin>>a;
+    for(int i=0;i<=10;i++)
+    {  b=a*i;
+      cout<<a<<" X "<<i<<" = "<<b<<endl;
+      
+      
+    }
+   return 0;
+}
+
