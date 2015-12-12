8981facedefd850dffb5364a5cdb07254ca41874
jemimacatherine <jemimacatherine@gmail.com>
2015-12-12 12:12:51 +0530

    Create primeinterval.c

---
+++ primeinterval.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int start,end,i,flag=0;
+printf("enter the starting and ending interval");
+scanf("%d%d",&start,&end);
+for(i=start;i<=end;i++)
+{
+
+}
+printf("the above is the prime number series");
+return 0;
